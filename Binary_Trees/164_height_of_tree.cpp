7b098ada9d96c8033b9cd3fde8e19625b276f4aa
Neha Kale <nkale8013@gmail.com>
2022-03-26 16:22:30 +0530

    height of tree

---
+++ Binary_Trees/164_height_of_tree.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int height(node* root){
+
+    if(root == NULL) return 0;
+    return 1 + max(height(root->left), height(root->right));
+}
+
+
+int main(){
+
+    node* root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    
+
+    cout<<height(root);
+    
+}
